5fbc8ce4300c0d9336dae14f1461a64f370fbbb5
Ayao <111835015+Ayao-AlwaysOnline@users.noreply.github.com>
2026-03-24 17:33:17 +0800

    Update Footer.js

--- themes/starter/components/Footer.js
+++ themes/starter/components/Footer.js
@@ -134,11 +134,11 @@ export const Footer = props => {
                   <p className='text-base text-gray-7'>
                     Designed and Developed by
                     <a
-                      href='https://github.com/tangly1024/NotionNext'
+                      href='https://seaportcy.com/studios'
                       rel='nofollow noopner noreferrer'
                       target='_blank'
                       className='px-1 text-gray-1 hover:underline'>
-                      NotionNext {siteConfig('VERSION')}
+                      SeaportcyStudios
                     </a>
                   </p>
                 </div>
