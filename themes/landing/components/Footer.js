8dc55f03768e8a771f633041958d168c9cce5da3
Ayao <111835015+Ayao-AlwaysOnline@users.noreply.github.com>
2026-05-07 20:52:00 +0800

    Update Footer.js

--- themes/landing/components/Footer.js
+++ themes/landing/components/Footer.js
@@ -40,7 +40,9 @@ export default function Footer() {
                 <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
 
                     {/* 1st block */}
-                  
+                    <div className="sm:col-span-12 lg:col-span-3">
+                        
+                    </div>
 
                     {/* 2nd block */}
                     <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
