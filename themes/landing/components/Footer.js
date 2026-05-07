9b4246d12758ce86ab16d50d9db5251c6108a088
Ayao <111835015+Ayao-AlwaysOnline@users.noreply.github.com>
2026-05-07 20:48:33 +0800

    Update Footer.js

--- themes/landing/components/Footer.js
+++ themes/landing/components/Footer.js
@@ -40,11 +40,7 @@ export default function Footer() {
                 <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
 
                     {/* 1st block */}
-                    <div className="sm:col-span-12 lg:col-span-3">
-                        <div className="mb-2">
-                            <Logo />
-                        </div>
-                    </div>
+                  
 
                     {/* 2nd block */}
                     <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
@@ -169,7 +165,7 @@ export default function Footer() {
                     </ul>
 
                     {/* Copyrights note */}
-                    <div className="text-sm text-gray-600 mr-4"><img src="images/starter/logo/logo.svg" className="seaportcyicon" alt="" />Seaportcy始终愿与您共同成长</div>
+                    <div className="text-sm text-gray-600 mr-4"><img src="images/starter/logo/logo.svg" className="seaportcyicon" alt="" />  Seaportcy始终愿与您共同成长</div>
 
                 </div>
 
