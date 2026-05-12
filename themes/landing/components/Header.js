1f4d2bc2d1b388dc6ef746b0199959d0a94a0521
Ayao <111835015+Ayao-AlwaysOnline@users.noreply.github.com>
2026-05-12 19:46:31 +0800

    Update Header.js

--- themes/landing/components/Header.js
+++ themes/landing/components/Header.js
@@ -41,11 +41,8 @@ export default function Header() {
                 </SmartLink>
               </li>
               <li>
-                <SmartLink href={siteConfig('LANDING_HEADER_BUTTON_2_URL', null, CONFIG)} target='_blank' className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                <SmartLink href={siteConfig('LANDING_HEADER_BUTTON_2_URL', null, CONFIG)} target='_blank' className="font-medium hover:font-bold text-gray-600 hover:text-gray-900 dark:text-gray-400 px-5 py-3 flex items-center transition duration-150 ease-in-out">
                   <span>{siteConfig('LANDING_HEADER_BUTTON_2_TITLE', null, CONFIG)}</span>
-                  <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
-                  </svg>
                 </SmartLink>
               </li>
             </ul>
