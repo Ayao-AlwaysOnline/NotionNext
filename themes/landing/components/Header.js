e61bfaaa7e7644a44953908aa1d1258a006c4263
Ayao <111835015+Ayao-AlwaysOnline@users.noreply.github.com>
2026-05-19 19:11:42 +0800

    Update Header.js

--- themes/landing/components/Header.js
+++ themes/landing/components/Header.js
@@ -48,8 +48,8 @@ export default function Header() {
                   <span>{siteConfig('LANDING_HEADER_BUTTON_2_TITLE', null, CONFIG)}</span>
                   <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
-                  </a>
-                </SmartLink>
+                  </svg>
+                </a>
               </li>
             </ul>
 
