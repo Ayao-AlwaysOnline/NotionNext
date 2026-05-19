a6762eb663f1658910c1c6bc65dffa5809d93cd6
Ayao <111835015+Ayao-AlwaysOnline@users.noreply.github.com>
2026-05-19 18:57:46 +0800

    Update MobileMenu.js

--- themes/landing/components/MobileMenu.js
+++ themes/landing/components/MobileMenu.js
@@ -67,7 +67,7 @@ export default function MobileMenu() {
         >
           <ul className="px-5 py-2">
             <li>
-              <a href="https://seaportcy.com/studios-en" target="_blank" rel="noopener noreferrer" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 w-full my-2 flex items-center justify-center">
+              <a href="https://seaportcy.com/studios-en" rel="noopener noreferrer" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 w-full my-2 flex items-center justify-center">
                 <span>English</span>
                 <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                   <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fill="#999" fillRule="nonzero" />
@@ -75,7 +75,7 @@ export default function MobileMenu() {
               </a>
             </li>
             <li>
-              <a href="https://seaportcy.com/studios-ja" target="_blank" rel="noopener noreferrer" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 w-full my-2 flex items-center justify-center">
+              <a href="https://seaportcy.com/studios-ja" rel="noopener noreferrer" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 w-full my-2 flex items-center justify-center">
                 <span>日本语</span>
                 <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                   <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fill="#999" fillRule="nonzero" />
