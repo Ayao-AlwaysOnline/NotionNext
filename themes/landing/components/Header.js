e975363a1297df87bd7b15293e712182878946a4
Ayao <111835015+Ayao-AlwaysOnline@users.noreply.github.com>
2026-05-21 18:48:02 +0800

    Update Header.js

--- themes/landing/components/Header.js
+++ themes/landing/components/Header.js
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
 import { siteConfig } from '@/lib/config'
 import SmartLink from '@/components/SmartLink'
 import Logo from './Logo'
@@ -10,15 +9,8 @@ import CONFIG from '../config'
 
 export default function Header() {
   const [top, setTop] = useState(true)
-  const router = useRouter();
-  const path = router.asPath;
 
-  // 根据当前路径判断是 packing 站还是 studios 站
-  const isPacking = path.startsWith('/packing');
-  const langBase = isPacking ? '/packing' : '/studios';
-  const enUrl = `https://seaportcy.com${langBase}-en`;
-  const jaUrl = `https://seaportcy.com${langBase}-ja`;
-
+  // detect whether user has scrolled the page down by 10px
   const scrollHandler = () => {
     window.pageYOffset > 10 ? setTop(false) : setTop(true)
   }
@@ -34,29 +26,33 @@ export default function Header() {
       <div className="max-w-6xl mx-auto px-5 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
 
+          {/* Site branding */}
           <div className="shrink-0 mr-4">
             <Logo />
           </div>
 
+          {/* Desktop navigation */}
           <nav className="hidden md:flex md:grow">
+            {/* Desktop sign in links */}
             <ul className="flex grow justify-end flex-wrap items-center">
               <li>
-                <a href={enUrl} className="btn-sm text-gray-200 hover:bg-gray-800 ml-3">
-                  <span>English</span>
-                  <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
-                  </svg>
+                <a href="https://seaportcy.com/studios-en" className="btn-sm text-gray-200 hover:bg-gray-800 ml-3">
+                   <span>{siteConfig('LANDING_HEADER_BUTTON_1_TITLE', null, CONFIG)}</span>
+                   <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
+                   </svg>
                 </a>
               </li>
               <li>
-                <a href={jaUrl} className="btn-sm text-gray-200 hover:bg-gray-800 ml-3">
-                  <span>日本语</span>
+                <a href="https://seaportcy.com/studios-ja" className="btn-sm text-gray-200 hover:bg-gray-800 ml-3">
+                  <span>{siteConfig('LANDING_HEADER_BUTTON_2_TITLE', null, CONFIG)}</span>
                   <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
                   </svg>
                 </a>
               </li>
             </ul>
+
           </nav>
 
           <MobileMenu />
